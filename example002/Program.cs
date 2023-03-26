c7b19061b396f745d9ce3c53397c6265561256df
Tyapor718 <katepsupport@mail.com>
2023-03-26 20:32:35 +0400

    Нахождение максимума среди трех чисел

---
+++ example002/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WriteLine("Введите первое число");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int N2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int N3 = Convert.ToInt32(Console.ReadLine());
+int max = N1;
+if (N2 >= max)
+{
+    max = N2;
+}
+if (N3 >= max)
+{
+    max = N3;
+}
+Console.WriteLine("Максимальное число " + max);
