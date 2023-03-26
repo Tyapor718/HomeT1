cb5f1678e767528e9c6d1d6b8149a17ceb6ff8dc
Tyapor718 <katepsupport@mail.com>
2023-03-26 20:46:41 +0400

    Показ всех четных от 0 до N

---
+++ example04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int A = 2;
+while (count < N/2)
+{
+    if (A%2 == 0)
+    {
+        Console.Write (A + " ");
+        A = A + 2;
+    }
+    else 
+    {
+        A = A + 1;
+    }
+count ++;
+}
